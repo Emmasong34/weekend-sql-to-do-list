9b812a75e288b55bf8bffc9315d01d93ba449313
Emma Song Fisher <emmasongf@gmail.com>
2020-10-17 13:06:37 -0500

    GET request on client, appendToDom function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,6 +4,7 @@ $(document).ready(onReady);
 
 function onReady(){
     console.log('hello from jq');
+    getTasks();
     $('#submitTask').on('click', addToList);
 }
 
@@ -17,5 +18,53 @@ function addToList(){
             data: newTask
         }).then(function (response){
             $('#newTask').val('');
+            ///get request will go here
+            getTasks();
+        }).catch(function(error) {
+            console.log(error);
         });
     };
+
+
+//gets tasks from server
+ function getTasks(){
+     console.log('in getTasks');
+     $.ajax({
+         method: 'GET',
+         url: '/taskList'
+     }).then(function(response){
+         appendToDom(response);
+     }).catch(function(error){
+         console.log(error);
+     });
+ };
+
+ //appends list to dom
+ function appendToDom(){
+     console.log('in appendToDom');
+     //empties so the list doesn't double
+     $('#newTask').empty();
+     console.log('array', array);
+     for(let i=0; i < array.length; i++){
+         let element = '';
+         if(array[i].completed === true){
+             element = 'Task completed';
+         } else {
+             element = '<button class ="taskCompleted">DONE</button>'
+         }
+         $('#list').append(`
+         <li data-id=${array[i].id}>
+            <p>${array[i].task}: <span>${element}</span> <p>
+            <button class="delete">delete</button>
+         </li>
+         `);
+     };
+ };
+
+
+
+
+
+     
+  
+  
