ccdb3b67e4e7d5344d2dc06a16453ad1b2174365
Emma Song Fisher <emmasongf@gmail.com>
2020-10-18 11:32:18 -0500

    newTask made into object in get request

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -17,7 +17,7 @@ function addToList(){
         $.ajax({
             type: 'POST',
             url: '/taskList',
-            data: newTask
+            data: {newTask: newTask}
         }).then(function (response){
             $('#taskInput').val('');
             ///get request will go here
@@ -29,48 +29,82 @@ function addToList(){
 
 
 //gets tasks from server
- function getTasks(){
-     console.log('in getTasks');
-     $.ajax({
-         method: 'GET',
-         url: '/taskList'
-     }).then(function(response){
-         appendToDom(response);
-     }).catch(function(error){
-         console.log(error);
-     });
- };
+//  function getTasks(){
+//      console.log('in getTasks');
+//      $.ajax({
+//          method: 'GET',
+//          url: '/taskList'
+//      }).then(function(response){
+//          appendToDom(response);
+//      }).catch(function(error){
+//          console.log(error);
+//      });
+//  };
+
+
+ function getTasks() {
+    $('#list').empty();
+    $.ajax({
+        type: 'GET',
+        url: '/taskList'
+    }).then(function (response) {
+        console.log("in new get", response);
+        // append data to the DOM
+        for (let i = 0; i < response.length; i++) {
+            $('#list').append(`
+                <ul>
+                    <li${response[i].id}>
+                        ${response[i].task}
+                        <button class ="taskCompleted">finished</button>
+                        <button class="delete">remove</button>
+            `);
+        }
+    });
+}
+
+
+
+
 
  //appends list to dom
- function appendToDom(array){
-     console.log('in appendToDom');
-     //empties so the list doesn't double
-     $('#taskInput').empty();
-     console.log('array', array);
-     for(let i=0; i < array.length; i++){
-         let element = '';
-         if(array[i].completed === true){
-             element = 'Task completed';
-         } else {
-             element = '<button class ="taskCompleted">DONE</button>'
-         }
-         $('#list').append(`
-         <li data-id=${array[i].id}>
-            ${array[i].task}: <span>${element}</span> 
-            <button class="delete">delete</button>
-         </li>
-         `);
-     };
- };
+//  function appendToDom(array){
+//      console.log('in appendToDom');
+//      //empties so the list doesn't double
+//      $('#list').empty();
+//      console.log('array', array);
+//      for(let i=0; i < array.length; i++){
+//          let element = '';
+//          if(array[i].completed === true){
+//              element = 'Task completed';
+//          } else {
+//              element = '<button class ="taskCompleted">finished</button>'
+//          }
+//          $('#list').append(`
+//          <li data-id=${array[i].id}>
+//             ${array[i].task} <span>${element}</span> 
+//             <button class="delete">remove</button>
+//          </li>
+//          `);
+//      };
+//  };
+
+ 
+
+
+
+
+
+
+
 
  //updates database 
 function updateTaskList(){
     let taskListId = $(this).closest('li').data('id');
-    console.log(taskListId);
+    console.log('in updateTaskList function');//this was the old console.log taskListId
     $.ajax({
         method: 'PUT',
         url: `/taskList/completed/${taskListId}`,
-        data: {completed: true}
+        data: {completedYet: true}
     }).then(function(response){
         console.log(response);
         getTasks();
@@ -83,7 +117,7 @@ function updateTaskList(){
 
 function deleteTask(){
     let taskListId = $(this).closest('li').data('id');
-    console.log(taskListId);
+    console.log('in deleteTask', taskListId);
     $.ajax({
         method: 'DELETE',
         url: `/taskList/${taskListId}`
--- server/routes/taskRouter.js
+++ server/routes/taskRouter.js
@@ -29,20 +29,41 @@ router.get('/', (req, res) => {
     });
 });
 
+router.put('/completed/:idParam', (req, res) => {
+    //let taskListId = req.params.id;
+    console.log('in put request', req.body.completedYet, req.params.id);
+    //create SQL query
+    let queryText = '';
+    if(req.body.completedYet === "true"){
+        queryText = `UPDATE "taskList" SET "completed" = "true" WHERE "id" = ${req.params.id};`;
+    } else {
+        queryText = `Not completed yet`
+    }
+    pool.query(queryText).then((result) => {
+        console.log('result from put', result);
+        res.sendStatus(200);
+     }).catch((error) => {
+        console.log('error in put', error);
+         res.sendStatus(500);
+     });
+});
+
+
+
 
 //updating the database 
-router.put('/completed/:id', (req, res) => {
-    let taskListId = req.params.id;
-    let completed = req.body.completed;
-    let queryText = `UPDATE "taskList" SET "completed" = $1 WHERE "id" = $2;`;
-    pool.query(queryText, [completed, taskListId]).then((result) => {
-        console.log(result);
-        res.sendStatus(200);
-    }).catch((error) => {
-        console.log('error in put', error);
-        res.sendStatus(500);
-    });
-});
+// router.put('/completed/:id', (req, res) => {
+//     let taskListId = req.params.id;
+//     let completed = req.body.completedYet;
+//     let queryText = `UPDATE "taskList" SET "completed" = $1 WHERE "id" = $2;`;
+//     pool.query(queryText, [completed, taskListId]).then((result) => {
+//         console.log(result);
+//         res.sendStatus(200);
+//     }).catch((error) => {
+//         console.log('error in put', error);
+//         res.sendStatus(500);
+//     });
+// });
 
 router.delete('/:id', (req, res) =>{
     let taskListId = req.params.id;
